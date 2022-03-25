87933c8e753047ee5cbd62480313f7ad96eb25cd
goDlin <godlin086@gmail.com>
2022-03-25 07:12:28 +0300

    captcha

--- pages/api/getTorrents.js
+++ pages/api/getTorrents.js
@@ -5,7 +5,8 @@ export default async (req, res) => {
 
   try {
     const rutracker = new RutrackerApi()
-    await rutracker.login({ username: process.env.RUTRACKER_LOGIN, password: process.env.RUTRACKER_PASSWORD })
+    const answer = await rutracker.getCaptcha()
+    await rutracker.login(process.env.RUTRACKER_LOGIN, process.env.RUTRACKER_PASSWORD, answer)
     const torrents = await rutracker.search({ query: 'Отец Father', sort: 'size' })
     console.log(torrents)
     res.status(200).json(torrents)
