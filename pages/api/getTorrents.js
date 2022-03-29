4add50b480f69cd9ecdd7960b66cc7ac2d32b5a8
goDlin <godlin086@gmail.com>
2022-03-29 19:14:11 +0300

    response type

--- pages/api/getTorrents.js
+++ pages/api/getTorrents.js
@@ -45,7 +45,8 @@ export default async (req, res) => {
         headers: {
           'Accept-Encoding': 'gzip, deflate, br',
           'Cookie': cookie, //+ ' opt_js={%22only_new%22:2}',
-          'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:83.0) Gecko/20100101 Firefox/83.0'
+          'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:83.0) Gecko/20100101 Firefox/83.0',
+          'Content-Type': 'application/json; application/octet-stream; application/x-www-form-urlencoded'
         }
       }
 
