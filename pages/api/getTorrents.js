d6a0c2000434af8a450314f0f94657b47dba06bc
goDlin <godlin086@gmail.com>
2022-03-29 06:47:57 +0300

    new rutracker login

--- pages/api/getTorrents.js
+++ pages/api/getTorrents.js
@@ -1,17 +1,20 @@
-import RutrackerApi from 'rutracker-api-2'
+//import RutrackerApi from 'rutracker-api'
+import Rutracker from '../../lib/rutracker'
 
 export default async (req, res) => {
   //const { year, month } = req.query
 
-  try {
-    const rutracker = new RutrackerApi()
-    const answer = await rutracker.getCaptcha()
-    await rutracker.login(process.env.RUTRACKER_LOGIN, process.env.RUTRACKER_PASSWORD, answer)
-    const torrents = await rutracker.search({ query: 'Отец Father', sort: 'size' })
-    console.log(torrents)
-    res.status(200).json(torrents)
-  }
-  catch (e) {
-    console.log(e)
-  }
+  // try {
+  //   const rutracker = new RutrackerApi()
+  //   await rutracker.login({ username: process.env.RUTRACKER_LOGIN, password: process.env.RUTRACKER_PASSWORD })
+  //   const torrents = await rutracker.search({ query: 'Отец Father', sort: 'size' })
+  //   console.log(torrents)
+  //   res.status(200).json(torrents)
+  // }
+  // catch (e) {
+  //   console.log(e)
+  // }
+
+  const rutrack = new Rutracker()
+  rutrack.login(process.env.RUTRACKER_LOGIN, process.env.RUTRACKER_PASSWORD)
 }
