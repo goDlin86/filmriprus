08832735f98dfc0dd190437c17ad667c42df628b
goDlin <godlin086@gmail.com>
2022-03-29 18:37:49 +0300

    response to browser

--- pages/api/getTorrents.js
+++ pages/api/getTorrents.js
@@ -1,8 +1,7 @@
-//import RutrackerApi from 'rutracker-api'
-import Rutracker from '../../lib/rutracker'
+import request from 'request'
 
 export default async (req, res) => {
-  //const { year, month } = req.query
+  //const { q } = req.query
 
   // try {
   //   const rutracker = new RutrackerApi()
@@ -15,6 +14,49 @@ export default async (req, res) => {
   //   console.log(e)
   // }
 
-  const rutrack = new Rutracker()
-  rutrack.login(process.env.RUTRACKER_LOGIN, process.env.RUTRACKER_PASSWORD)
+  const options = {
+    url: 'http://rutracker.org/forum/login.php', 
+    form: {
+      login_username: process.env.RUTRACKER_LOGIN,
+      login_password: process.env.RUTRACKER_PASSWORD,
+      login: 'Вход' //'%C2%F5%EE%E4'
+    },
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:83.0) Gecko/20100101 Firefox/83.0',
+      'Accept-Encoding': 'gzip, deflate, br'
+    }
+  }
+
+  request.post(options, (error, response, body) => {
+    if (error) {
+      console.log(error)
+    } else {
+      const [cookie] = response.headers['set-cookie'][0].split(';')
+      console.log(cookie)
+
+      const options = {
+        url: 'http://rutracker.org/forum/tracker.php',
+        form: {
+          nm: 'Отец Father',
+          o: '4', //sort downloads
+          s: '1' //asc order
+        },
+        responseType: 'arraybuffer',
+        headers: {
+          'Accept-Encoding': 'gzip, deflate, br',
+          'Cookie': cookie, //+ ' opt_js={%22only_new%22:2}',
+          'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:83.0) Gecko/20100101 Firefox/83.0'
+        }
+      }
+
+      request.post(options, (error, response, body) => {
+        if (error) {
+          console.log(error)
+        } else {
+          console.log(response)
+          res.status(200).json(response)
+        }
+      })
+    }
+})
 }
