d130ac3971fb6ca33d9d2a55e289e5671c3003d1
Denis <godlin086@gmail.com>
2023-02-16 18:49:39 +0300

    fix inngest file

--- pages/api/inngest.js
+++ pages/api/inngest.js
@@ -1,6 +1,5 @@
 import { Inngest } from 'inngest'
 import { serve } from 'inngest/next'
-import dayjs from 'dayjs'
 
 export const inngest = new Inngest({ name: 'filmriprus' })
 
@@ -8,11 +7,11 @@ const getFilms = inngest.createFunction(
   { name: 'Get films' }, 
   { cron: '0 12 * * 3' }, 
   async ({ event, step }) => {
-    const month = dayjs().format('MMMM')
-    const year = dayjs().format('YYYY')
+    const month = new Date().toLocaleString('en-EN', { month: 'long' })
+    const year = new Date().getFullYear()
     const res = await fetch(`https://filmriprus.vercel.app/api/filmsdb?year=${year}&month=${month}&secret=` + process.env.FILMRIP_TOKEN)
     return await res.json()
   }
 )
 
-export default serve(inngest, [ getAlbums ])
+export default serve(inngest, [ getFilms ])
